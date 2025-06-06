bd0c2d777aa9de2d13e6a404ee07f594c718f9ec
bkopilov <bkopilov@redhat.com>
2025-06-06 09:45:54 +0300

    fix unzip

--- api_tests/common/oracle21c_rac/binaries_management.py
+++ api_tests/common/oracle21c_rac/binaries_management.py
@@ -20,6 +20,7 @@ class Binaries21cRac(BinariesManagement):
     @classmethod
     def unzip_database_binary(cls, url):
         return f"""
+        echo "12345678" | su - oracle bash -c "mkdir -p /u01/app/oracle/product/21.0.0.0/dbhome_1"
         echo "12345678" | su - oracle bash -c "unzip /tmp/{url.split("/")[-1]} -d /u01/app/oracle/product/21.0.0.0/dbhome_1"
         """
 
