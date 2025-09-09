134f6f5226aa675e9012b5eb6b667e7d34586e2a
bkopilov <bkopilov@redhat.com>
2025-09-09 14:52:57 +0300

    add operators

--- api_tests/test_rac_on_vms_deployment.py
+++ api_tests/test_rac_on_vms_deployment.py
@@ -27,7 +27,7 @@ CPU_CORES = 60
 RAM_MEMORY_GIB = 1024 * 60
 DISK_COUNT = 2
 VIRTUALIZATION_BUNDLE = ['odf', 'cnv', 'self-node-remediation', 'lso', 'nmstate', 'kube-descheduler',
--                         'node-healthcheck', 'fence-agents-remediation', 'node-maintenance']
+                         'node-healthcheck', 'fence-agents-remediation', 'node-maintenance']
 APPLY_ACTION_TIMEOUT = 30
 
 
