bb0149131196670ada8fe2310195673ddb097ffc
bkopilov <bkopilov@redhat.com>
2025-09-13 08:34:18 +0300

    ignore err

--- api_tests/common/commands/node_commands.py
+++ api_tests/common/commands/node_commands.py
@@ -42,14 +42,15 @@ class NodeSshHandler(SshConnection):
         error = stderr.read()
         status = stdout.channel.recv_exit_status()
 
-        output_decode = output.decode('utf-8')
+        output = output.decode('utf-8', errors='ignore')
         if status != 0:
             e = RuntimeError(
-                f"Failed executing, status '{status}', output was:\n{output} stderr \n{error}"
+                f"Failed executing, status '{status}', output was:\n{output} stderr \n"
+                f"{error.decode('utf-8', errors='ignore')}"
             )
             e.output = output
             raise e
-        return output_decode
+        return output
 
     def upload_file(self, local_source_path, remote_target_path):
         super().upload_file(local_source_path, remote_target_path)
