3d40e5c5724da1040a029a5de56eeb5f65b4a420
bkopilov <bkopilov@redhat.com>
2025-09-13 15:33:32 +0300

    update mem

--- api_tests/test_rac_on_vms_deployment.py
+++ api_tests/test_rac_on_vms_deployment.py
@@ -24,8 +24,8 @@ import pytest
 import time
 logger = logging.getLogger(__name__)
 
-CPU_CORES = 50
-RAM_MEMORY_GIB = 1024 * 50
+CPU_CORES = 70
+RAM_MEMORY_GIB = 1024 * 70
 DISK_COUNT = 2
 VIRTUALIZATION_BUNDLE = ['odf', 'cnv', 'lso', 'nmstate']
 APPLY_ACTION_TIMEOUT = 10
