abf26510fe145b1785bf1b15ccf81fdeb378dc5e
bkopilov <bkopilov@redhat.com>
2025-09-15 19:21:57 +0300

    update run shell

--- api_tests/common/commands/shell_commands.py
+++ api_tests/common/commands/shell_commands.py
@@ -5,10 +5,13 @@ import subprocess
 logger = logging.getLogger(__name__)
 
 
-def run_shell_command(cmd):
+def run_shell_command(cmd, shell=True):
     cmd = cmd.split()
     logger.info(f"run_shell_command {cmd}")
     try:
-        subprocess.run(cmd)
+        process = subprocess.run(cmd, shell=shell, stdout=subprocess.PIPE, universal_newlines=True)
+        output = process.stdout.strip()
+        return output
     except subprocess.CalledProcessError as e:
         logger.error(e)
+        return None
