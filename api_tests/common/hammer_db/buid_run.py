67f13b59e2db0913d17a5ed26052332c3113b089
bkopilov <bkopilov@redhat.com>
2025-09-25 13:27:28 +0300

    update hammerdb

--- api_tests/common/hammer_db/buid_run.py
+++ api_tests/common/hammer_db/buid_run.py
@@ -5,18 +5,18 @@ class BuildRunHammerDB(BuildRun):
     @classmethod
     def env_params(cls):
         return """
-        bash -c "cat > /opt/HammerDB-5.0/env_ << EOF
+        bash -c "cat > /opt/HammerDB-5.0/.env << EOF
 export TNS_ADMIN=/opt/HammerDB-5.0/
 export ORACLE_SYSTEM_USER=system
 export ORACLE_SYSTEM_PASSWORD=Password1
 export ORACLE_INSTANCE=oralab
-ORACLE_HOME=/usr/lib/oracle/21/client64
-
+export ORACLE_HOME=/usr/lib/oracle/21/client64/usr/lib/oracle/21/client64
+export TMP=/tmp/
 #small profile
-export ORA_NUM_VU=1
-export ORA_COUNT_WARE=1
+export ORA_NUM_VU=2
+export ORA_COUNT_WARE=4
 export ORA_RAMPUP=2
-export ORA_DURATION=2
+export ORA_DURATION=5
 export ORA_ALLWAREHOUSE=false
 export HDB_PROFILE_ID=100
 EOF
@@ -26,20 +26,20 @@ EOF
     @classmethod
     def build_hammerbd(cls):
         return """
-        bash -c "source /opt/HammerDB-5.0/env_ && /opt/HammerDB-5.0/hammerdbcli auto build.tcl"
+        bash -c "source /opt/HammerDB-5.0/.env && /opt/HammerDB-5.0/hammerdbcli auto build.tcl"
         :return:
         """
 
     @classmethod
     def run_hammerbd(cls):
         return """
-        bash -c "source /opt/HammerDB-5.0/env_ && /opt/HammerDB-5.0/hammerdbcli auto run.tcl"
+        bash -c "source /opt/HammerDB-5.0/.env && /opt/HammerDB-5.0/hammerdbcli auto run.tcl"
         :return:
         """
 
     @classmethod
     def drop_hammerbd(cls):
         return """
-        bash -c "source /opt/HammerDB-5.0/env_ && /opt/HammerDB-5.0/drop_tpcc_user.sh"
+        bash -c "source /opt/HammerDB-5.0/.env && /opt/HammerDB-5.0/drop_tpcc_user.sh"
         """
 
--- api_tests/templates/hammer_db/run.tcl
+++ api_tests/templates/hammer_db/run.tcl
@@ -1,7 +1,7 @@
 puts "Starting Oracle TPCC benchmark run..."
 
 # Oracle environment variables
-set env(ORACLE_HOME) "/usr/lib/oracle/19.26/client64"
+set env(ORACLE_HOME) "/usr/lib/oracle/21/client64"
 set env(LD_LIBRARY_PATH) "$env(ORACLE_HOME)/lib"
 
 # Wait for all Virtual Users to finish
