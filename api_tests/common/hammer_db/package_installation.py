ef0b8c3b7776262f42ac35cf66ad796354a0979a
bkopilov <bkopilov@redhat.com>
2025-10-05 22:21:17 +0300

    update hammerdb version

--- api_tests/common/hammer_db/package_installation.py
+++ api_tests/common/hammer_db/package_installation.py
@@ -7,7 +7,14 @@ class PackageInstallHammer5(PackageInstallation):
     def package_pre_install(cls):
         # install hammer-db inside the running container
         return """
-   dnf install -y https://github.com/TPC-Council/HammerDB/releases/download/v5.0/hammerdb-5.0-1.el9.x86_64.rpm
+   VERSION=`grep "VERSION_ID" /etc/os-release | cut -d'=' -f2 | tr -d '"'`
+   if [ $VERSION == "9.6" ]; then
+    dnf install -y https://github.com/TPC-Council/HammerDB/releases/download/v5.0/hammerdb-5.0-1.el9.x86_64.rpm
+   elif [ $VERSION == "8.6" ]; then
+    dnf install -y https://github.com/TPC-Council/HammerDB/releases/download/v5.0/hammerdb-5.0-1.el8.x86_64.rpm
+   else
+    echo "hammerdb version not supported"
+   fi
    dnf install -y https://download.oracle.com/otn_software/linux/instantclient/2118000/oracle-instantclient-basic-21.18.0.0.0-1.el8.x86_64.rpm
    dnf install -y https://download.oracle.com/otn_software/linux/instantclient/2118000/oracle-instantclient-sqlplus-21.18.0.0.0-1.el8.x86_64.rpm
 
