c8b18e3c9935493102d409dd6b18826b2ef17725
bkopilov <bkopilov@redhat.com>
2025-10-07 10:04:54 +0300

    hammerdb from container

--- api_tests/common/hammer_db/package_installation.py
+++ api_tests/common/hammer_db/package_installation.py
@@ -7,8 +7,8 @@ class PackageInstallHammer5(PackageInstallation):
     def package_pre_install(cls):
         # install hammer-db inside the running container
         return """
-    # dnf install -y https://github.com/TPC-Council/HammerDB/releases/download/v5.0/hammerdb-5.0-1.el9.x86_64.rpm
-    dnf install -y https://github.com/TPC-Council/HammerDB/releases/download/v5.0/hammerdb-5.0-1.el8.x86_64.rpm
+    dnf install -y https://github.com/TPC-Council/HammerDB/releases/download/v5.0/hammerdb-5.0-1.el9.x86_64.rpm
+    # dnf install -y https://github.com/TPC-Council/HammerDB/releases/download/v5.0/hammerdb-5.0-1.el8.x86_64.rpm
    
     dnf install -y https://download.oracle.com/otn_software/linux/instantclient/2118000/oracle-instantclient-basic-21.18.0.0.0-1.el8.x86_64.rpm
     dnf install -y https://download.oracle.com/otn_software/linux/instantclient/2118000/oracle-instantclient-sqlplus-21.18.0.0.0-1.el8.x86_64.rpm
