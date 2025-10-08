d345375ca4b69aea1fca46666bdb6c32078a7291
bkopilov <bkopilov@redhat.com>
2025-10-09 00:31:30 +0300

    output shell

--- api_tests/common/commands/shell_commands.py
+++ api_tests/common/commands/shell_commands.py
@@ -2,6 +2,8 @@ import logging
 
 import subprocess
 
+from datetime import datetime
+
 logger = logging.getLogger(__name__)
 
 
@@ -10,6 +12,7 @@ def run_shell_command(cmd, shell=True):
     try:
         process = subprocess.run(cmd, shell=shell, stdout=subprocess.PIPE, universal_newlines=True)
         output = process.stdout.strip()
+        logging.info(f'\n {datetime.now()}|>>>\n{output}\n---')
         return output
     except subprocess.CalledProcessError as e:
         logger.error(e)
