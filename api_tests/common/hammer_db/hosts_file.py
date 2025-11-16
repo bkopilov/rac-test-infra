d3390f08b8a826f354dd2739bedc83bc7898445c
bkopilov <bkopilov@redhat.com>
2025-11-16 14:56:04 +0200

    fix /etc/resolv.conf

--- api_tests/common/hammer_db/hosts_file.py
+++ api_tests/common/hammer_db/hosts_file.py
@@ -8,7 +8,7 @@ class HostFileSetting(HostFile):
     def set_resolv_file(cls):
         return """
         bash -c "cat > /tmp/resolv.conf <<EOF
-        nameserver 192.168.120.1
+nameserver 192.168.120.1
 EOF"
 bash -c "cp /tmp/resolv.conf  /etc/resolv.conf"
 
